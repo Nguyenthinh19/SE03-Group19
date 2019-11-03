6389cf0bda4b1e965ac0739b3f6bb68cf7007723
Thắng Nguyễn Văn <thang.120598@gmail.com>
2019-11-03 14:54:07 +0700

    Giao diện trang signin, signup

---
+++ app/src/main/java/com/example/orderfoodsapp/Button/SignUpButton.java
@@ -0,0 +1,4 @@
+package com.example.orderfoodsapp.Button;
+
+public class SignUpButton {
+}
--- app/src/main/java/com/example/orderfoodsapp/MainActivity.java
+++ app/src/main/java/com/example/orderfoodsapp/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.orderfoodsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button btnSignUp, btnSignIn;
+        TextView txtSlogan;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            btnSignIn = (Button) findViewById(R.id.btnSignIn);
+            btnSignUp = (Button) findViewById(R.id.btnSignUp);
+            txtSlogan = (TextView) findViewById(R.id.txtSlogan);
+
+//            btnSignUp.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent SignUp = new Intent(MainActivity.this, SignUp.class);
+//                    startActivity(SignUp);
+//                }
+//            });
+//
+//            btnSignIn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent SignIN = new Intent(MainActivity.this, SignIn.class);
+//                    startActivity(SignIN);
+//                }
+//            });
+
+        }
     }
-}
